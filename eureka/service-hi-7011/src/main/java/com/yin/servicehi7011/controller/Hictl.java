ae597024f14b1510caa7dc8d07a29997d28b8792
yinfq <1257791382@qq.com>
2019-05-20 21:42:42 +0800

    t

--- eureka/service-hi-7011/src/main/java/com/yin/servicehi7011/controller/Hictl.java
+++ eureka/service-hi-7011/src/main/java/com/yin/servicehi7011/controller/Hictl.java
@@ -1,6 +1,7 @@
 package com.yin.servicehi7011.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * To change this template use File | Settings | File Templates.
  */
 @Controller
+@RefreshScope //开启更新功能
 public class Hictl {
     @Value("${server.port}")
     private String port;
